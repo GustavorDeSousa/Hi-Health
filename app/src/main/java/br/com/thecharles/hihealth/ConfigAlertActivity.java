6afabbad3408a53842199fe6231838b468beacdf
mateus borgonovi <mateus_borg9@hotmail.com>
2018-07-24 20:29:11 -0300

    Tela de Opções de Alerta + Botão de teste envio SMS

--- app/src/main/java/br/com/thecharles/hihealth/ConfigAlertActivity.java
+++ app/src/main/java/br/com/thecharles/hihealth/ConfigAlertActivity.java
@@ -2,6 +2,8 @@ package br.com.thecharles.hihealth;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.View;
 
 public class ConfigAlertActivity extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class ConfigAlertActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_config_alert);
+    }
+
+    public void enviarSms(View view) {
+        SmsManager smsManager = SmsManager.getDefault();
+        smsManager.sendTextMessage(
+                "011995083035",
+                null,
+                "Teste Aplicativo Hi-Health",
+                null,
+                null
+        );
     }
 }
