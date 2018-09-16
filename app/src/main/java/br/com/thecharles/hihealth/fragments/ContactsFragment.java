1fcce74fc8fcb003fb0df41d137b30178ad6ddd6
SeuCaiOo <caiopimentel8@gmail.com>
2018-09-16 16:55:47 -0300

    Add extras

--- app/src/main/java/br/com/thecharles/hihealth/fragments/ContactsFragment.java
+++ app/src/main/java/br/com/thecharles/hihealth/fragments/ContactsFragment.java
@@ -1,8 +1,13 @@
 package br.com.thecharles.hihealth.fragments;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -10,7 +15,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Switch;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -24,6 +33,7 @@ import java.util.ArrayList;
 
 import br.com.thecharles.hihealth.R;
 import br.com.thecharles.hihealth.activity.ChatActivity;
+import br.com.thecharles.hihealth.activity.LoginActivity;
 import br.com.thecharles.hihealth.adapter.ContactsAdapter;
 import br.com.thecharles.hihealth.config.SettingsFirebase;
 import br.com.thecharles.hihealth.helper.RecyclerItemClickListener;
@@ -54,6 +64,49 @@ public class ContactsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_contacts, container, false);
+
+        FloatingActionButton fab = view.findViewById(R.id.fab_add_contact);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+
+
+
+                AlertDialog.Builder mBuilder = new AlertDialog.Builder(getActivity());
+
+                //Pegando o dialog
+                View mView = getLayoutInflater().inflate(R.layout.dialog_add_contact, null);
+
+                //Buttons do Dialog
+                ImageView mSearch =  mView.findViewById(R.id.btnSearch);
+                final EditText editText  = mView.findViewById(R.id.edtAddContact);
+
+
+                mBuilder.setView(mView);
+                final AlertDialog dialog = mBuilder.create();
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+
+                mSearch.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String userAdd = editText.getText().toString();
+
+                        if( !userAdd.isEmpty() ) {//verifica e-mail
+                            dialog.dismiss();
+                            findUser();
+                        } else {
+                            Toast.makeText(getActivity(),
+                                    "Preencha o email!",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+            }
+        });
 
         //Configurações iniciais
 //        FirebaseUser userCurrent = firebaseAuth.getCurrentUser();
@@ -103,6 +156,12 @@ public class ContactsFragment extends Fragment {
                 ));
 
         return view;
+    }
+
+    private void findUser() {
+        Toast.makeText(getActivity(),
+                "Pesquisando usário!",
+                Toast.LENGTH_SHORT).show();
     }
 
     @Override
--- app/src/main/java/br/com/thecharles/hihealth/fragments/DataFragment.java
+++ app/src/main/java/br/com/thecharles/hihealth/fragments/DataFragment.java
@@ -1,19 +1,36 @@
 package br.com.thecharles.hihealth.fragments;
 
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import br.com.thecharles.hihealth.R;
+import br.com.thecharles.hihealth.activity.MainActivity;
 
 public class DataFragment extends Fragment{
 
     private SwipeRefreshLayout refreshLayout;
+
+    TextView countdownText;
+
+    CountDownTimer countDownTimer;
+    long timeLeftInMilleSeconds = 30000;
+    boolean timeRunning;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -23,7 +40,116 @@ public class DataFragment extends Fragment{
         refreshLayout = v.findViewById(R.id.swipe);
         refreshLayout.setOnRefreshListener(OnRefreshListener());
 
+        FloatingActionButton fab = v.findViewById(R.id.fab_warning);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder mBuilder = new AlertDialog.Builder(getActivity());
+
+                //Pegando o dialog
+                View mView = getLayoutInflater().inflate(R.layout.dialog_alert, null);
+
+                //Buttons do Dialog
+                Button mEnviar =  mView.findViewById(R.id.btnEnviar);
+                Button mCancelar =  mView.findViewById(R.id.btnCancelar);
+
+
+                mBuilder.setView(mView);
+                final AlertDialog dialog = mBuilder.create();
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+
+                mEnviar.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Toast.makeText(getActivity(), "Alerta enviada com sucesso", Toast.LENGTH_SHORT).show();
+
+                        AlertDialog.Builder mBuilder = new AlertDialog.Builder(getActivity());
+
+                        //Pegando o dialog
+                        View mView = getLayoutInflater().inflate(R.layout.dialog_sensor, null);
+
+
+                        //Button do Dialog
+                        Button mBem = mView.findViewById(R.id.btnBem);
+//                        Button mMal = mView.findViewById(R.id.btnMal);
+                        countdownText = mView.findViewById(R.id.countdown_text);
+
+                        mBuilder.setView(mView);
+                        final AlertDialog dialog = mBuilder.create();
+                        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                        dialog.show();
+
+                        //Metodo para inicializar o temporizador
+                        //startTimer();
+                        startTimer();
+
+
+
+
+                        mBem.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                Toast.makeText(getActivity(), "Alerta Emitido", Toast.LENGTH_SHORT).show();
+                                dialog.dismiss();
+                            }
+                        });
+
+//                        mMal.setOnClickListener(new View.OnClickListener() {
+//                            @Override
+//                            public void onClick(View view) {
+//                                Toast.makeText(getActivity(), "Alerta Cancelado", Toast.LENGTH_SHORT).show();
+//                                dialog.dismiss();
+//                            }
+//                        });
+
+                    }
+                });
+
+                mCancelar.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        dialog.dismiss();
+                    }
+                });
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
         return  v;
+    }
+
+    private void startTimer() {
+        countDownTimer = new CountDownTimer(timeLeftInMilleSeconds, 1000) {
+            @Override
+            public void onTick(long l) {
+                timeLeftInMilleSeconds = l;
+                updateTimer();
+            }
+
+            @Override
+            public void onFinish() {
+
+            }
+        }.start();
+        timeRunning = true;
+    }
+
+    private void updateTimer() {
+        int minutes = (int) timeLeftInMilleSeconds / 60000;
+        int seconds = (int) timeLeftInMilleSeconds % 60000 / 1000;
+
+        String timeLeftText;
+
+        timeLeftText = "" + minutes;
+        timeLeftText += ":";
+
+        if (seconds < 10 ) timeLeftText += "0";
+
+        timeLeftText += seconds;
+
+        countdownText.setText(timeLeftText);
     }
 
     private SwipeRefreshLayout.OnRefreshListener OnRefreshListener() {
@@ -73,5 +199,10 @@ public class DataFragment extends Fragment{
 
     public static DataFragment newInstance() {
         return new DataFragment();
+    }
+
+
+    public void onDialog(View view) {
+
     }
 }
