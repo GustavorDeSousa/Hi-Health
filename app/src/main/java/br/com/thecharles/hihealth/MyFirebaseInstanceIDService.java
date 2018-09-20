93b7dc9240e4b4812bde8c5ec438c98c6fe9f090
johnrocha <johnerickrocha@gmail.com>
2018-09-20 17:08:51 -0300

    Tutorial messages data

--- app/src/main/java/br/com/thecharles/hihealth/MyFirebaseInstanceIDService.java
+++ app/src/main/java/br/com/thecharles/hihealth/MyFirebaseInstanceIDService.java
@@ -7,9 +7,19 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.FirebaseInstanceIdService;
 
+import java.io.IOException;
+
 import br.com.thecharles.hihealth.config.SettingsFirebase;
 import br.com.thecharles.hihealth.helper.UserFirebase;
 import br.com.thecharles.hihealth.model.User;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
 
 public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
 
@@ -18,6 +28,8 @@ public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
     private FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
 
     private static final String TAG = FirebaseInstanceIdService.class.getSimpleName();
+    private static final String SEND_TOKEN_SERVICE_URL =
+            "http://yourAppServer/instanceTokenService";
 
     @Override
     public void onTokenRefresh() {
@@ -55,4 +67,38 @@ public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
     private void sendRegistrationToServer(String token) {
         // TODO: Implement this method to send token to your app server.
     }
+
+
+    private void sendInstanceIdTokenToServer(String token) {
+        OkHttpClient httpClient = new OkHttpClient();
+
+        RequestBody formBody = new FormBody.Builder()
+                .add("token", token)
+                .build();
+        Request request = new Request.Builder()
+                .url(SEND_TOKEN_SERVICE_URL)
+                .post(formBody)
+                .build();
+
+
+        httpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.e(TAG, "error sending firebase app instance token to app server");
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                ResponseBody responseBody = response.body();
+                if (!response.isSuccessful()) {
+                    throw new IOException
+                            ("Firebase app instance token to server status " + response);
+                }
+
+                Log.i(TAG, "Firebase app instance token has been sent to app server "
+                        +responseBody.string());
+            }
+        });
+    }
 }
+
--- app/src/main/java/br/com/thecharles/hihealth/MyFirebaseMessagingService.java
+++ app/src/main/java/br/com/thecharles/hihealth/MyFirebaseMessagingService.java
@@ -1,8 +1,64 @@
 package br.com.thecharles.hihealth;
 
-import android.app.Service;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import java.util.Map;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    private static final String TAG = "FcmDeviceSpecificMsgSer";
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        Log.d(TAG, "Message from server " + remoteMessage.getFrom());
+
+        if (remoteMessage.getData().size() > 0) {
+            sendNotification(null, remoteMessage.getData());
+        }
+        if (remoteMessage.getNotification() != null) {
+            sendNotification(remoteMessage.getNotification(), null);
+        }
+    }
+
+    private void sendNotification(RemoteMessage.Notification notification,
+                                  Map<String, String> data) {
+        String title;
+        String body;
+
+        if(notification == null){
+            title = "New Item Details";
+            body = data.get("itemName")+" "+data.get("itemPrice")+" "+data.get("location")
+//                    + " = " + data.get("userName")
+            ;
+        }else{
+            title = notification.getTitle();
+            body  = notification.getBody();
+        }
+
+
+//        Intent intent = new Intent(this,  DeviceSepecificMessageFCMActivity.class);
+//        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent,
+//                PendingIntent.FLAG_ONE_SHOT);
+
+        NotificationCompat.Builder notificationBuilder =
+                new NotificationCompat.Builder(this, "fcm-instance-specific")
+                        .setSmallIcon(R.drawable.ic_favorite_black_24dp)
+                        .setContentTitle(title)
+                        .setContentText(body)
+                        .setAutoCancel(true)
+//                        .setContentIntent(pendingIntent)
+                ;
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0, notificationBuilder.build());
+    }
 }
